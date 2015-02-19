51d15d15520985a911fa7aa9ea38414bdad2d14a
Tobias Oetzel <tobias.oetzel@sap.com>
2015-02-19 15:59:46 +0100

    update from ui5 - myrepo

---
+++ test/unit/allTests.js
@@ -0,0 +1,5 @@
+jQuery.sap.require("test.unit.model.Device");
+jQuery.sap.require("test.unit.model.AppModel");
+jQuery.sap.require("test.unit.model.formatter");
+jQuery.sap.require("test.unit.model.grouper");
+jQuery.sap.require("test.unit.controller.ListSelector");
